2000a9165179d94226b1e22f9049990cb0f53e6b
Mst.Bithy <mstbithy@outlook.com>
2023-06-11 18:34:55 +0600

    modify

--- BlazorFullStackCrud/Client/Services/SuperHeroService/SuperHeroService.cs
+++ BlazorFullStackCrud/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -1,69 +1,67 @@
-﻿using System.Net.Http.Json;
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace BlazorFullStackCrud.Client.Services.SuperHeroService
 {
-   public class SuperHeroService : ISuperHeroService
-   {
-      private readonly HttpClient _http;
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
 
-      public SuperHeroService(HttpClient http)
-      {
-         _http = http;
-      }
+        public SuperHeroService(HttpClient http, NavigationManager navigationManager)
+        {
+            _http = http;
+            _navigationManager = navigationManager;
+        }
 
-      public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
-      public List<Comic> Comics { get; set; } = new List<Comic> { };
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic>();
 
-      public async Task GetComics()
-      {
-         var result = await _http.GetFromJsonAsync<List<Comic>>("api/superhero/comics");
+        public async Task CreateHero(SuperHero hero)
+        {
+            var result = await _http.PostAsJsonAsync("api/superhero", hero);
+            await SetHeroes(result);
+        }
 
-         if (result != null)
-            Comics = result;
-      }
+        private async Task SetHeroes(HttpResponseMessage result)
+        {
+            var response = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
+            Heroes = response;
+            _navigationManager.NavigateTo("superheroes");
+        }
 
-      public async Task<SuperHero> GetSingleHero(int id)
-      {
-         var result = await _http.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+        public async Task DeleteHero(int id)
+        {
+            var result = await _http.DeleteAsync($"api/superhero/{id}");
+            await SetHeroes(result);
+        }
 
-         if (result != null)
-            return result;
+        public async Task GetComics()
+        {
+            var result = await _http.GetFromJsonAsync<List<Comic>>("api/superhero/comics");
+            if (result != null)
+                Comics = result;
+        }
 
-         throw new Exception("Hero not found");
-      }
+        public async Task<SuperHero> GetSingleHero(int id)
+        {
+            var result = await _http.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+            if (result != null)
+                return result;
+            throw new Exception("Hero not found!");
+        }
 
-      public async Task GetSuperHeroes()
-      {
-         var result = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+        public async Task GetSuperHeroes()
+        {
+            var result = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+            if (result != null)
+                Heroes = result;
+        }
 
-         if (result != null)
-            Heroes = result;
-      }
-
-      public async Task CreateHero(SuperHero hero)
-      {
-         var result = await _http.PostAsJsonAsync("api/superhero", hero);
-         await SetHeroes(result);
-      }
-
-      private async Task SetHeroes(HttpResponseMessage result)
-      {
-         var response = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
-         await SetHeroes(result);
-      }
-
-      public async Task UpdateHero(SuperHero hero)
-      {
-         var result = await _http.PostAsJsonAsync($"api/superhero/{hero.Id}", hero);
-         var response = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
-         await SetHeroes(result);
-      }
-
-      public async Task DeleteHero(int id)
-      {
-         var result = await _http.DeleteAsync($"api/superhero/{id}");
-         var response = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
-         await SetHeroes(result);
-      }
-   }
+        public async Task UpdateHero(SuperHero hero)
+        {
+            var result = await _http.PutAsJsonAsync($"api/superhero/{hero.Id}", hero);
+            await SetHeroes(result);
+        }
+    }
 }
